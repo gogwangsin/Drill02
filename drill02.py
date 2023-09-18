a151722e61b910eae38935a151b1cf1835a3b36e
gogwangsin <gwangsin1029@naver.com>
2023-09-18 21:23:49 +0900

    최종 제출2
    캐릭터 픽셀 정보 가로 42, 세로 92에서 피봇 위치 중아인 21,46을 기준으로 사각형 범위를 그리고 ( 처음 제출 땐 &&연산을 파이썬에서 어떻게 쓰는지 몰라서 못 했습니다 ) 원 그리기는 캐릭터가 중앙에 있을 때 그 위치를 화면 중앙 400,300을 원점으로 하는 반지름 200의 원의 2/3파이 지점에 있다고 생각할 수 있고 단위원 좌표처럼 x,y를 대응시키는데 초기위치 2/3파이를 한바퀴 지난 2/3파이 + 2파이가 됐을 때 조건들을 초기화 시킵니다.

--- drill02.py
+++ drill02.py
@@ -1,37 +1,88 @@
 from pico2d import *
-
+import os
+os.chdir('C:\\Drill\Drill02')
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 x = 0
 y = 90
-def drawRec():
+isRecCicle = False
+isCircle = False
+# 800, 600
+# 42, 92 중앙 21,46
+def moveRec():
     global x
     global y
+    global isRecCicle
+    global isCircle
     
-    if x >= 790 and y < 550:
-        y = y + 2
-    elif x < 790:
-        x = x + 2
+    if((x >= 0 and x < 758) and (y <= 90 and y > 0)):
+        if x > 399 and isRecCicle == True:
+            isCircle = True
+            return False
+        x += 2
+    elif (( x >= 758) and (y <= 600 - 46 )): # 전체 599 - 캐릭피봇y + 땅위치
+        y += 2
+    elif ((x >= 2 ) and (y > 600 - 46 )):
+        x -= 2
+    elif ((x < 21 ) and (y > 90)):
+        y -= 2
+        isRecCicle = True
 
-    if y >= 550:
-        x = x - 4
-        if x < 80:
-            y = y-4
-    elif y > 90 and x < 80:
-        y = y -4
+import math
 
+seta_pi = math.pi * 3/2 # 2분의 3pi가 초기값 
+
+
+def moveCircle():
+    global x
+    global y
+    global seta_pi
+    global isCircle
+    global isRecCicle
+    circle_x = 400
+    circle_y = 300
     
+    x =  circle_x + 200*math.cos(seta_pi)
+    y =  circle_y + 200*math.sin(seta_pi)
+    seta_pi = seta_pi + 0.01
+
+    if(seta_pi > 2*math.pi + math.pi * 3/2):
+        seta_pi = math.pi * 3/2
+        x = 400
+        y = 90
+        isCircle = False
+        isRecCicle = False
 
 
-        
-    
 while( x < 800 ):
     clear_canvas_now()
     grass.draw_now(400,30)
     character.draw_now(x,y)
-    drawRec()
-    delay(0.01)
+
+    if(moveRec()):
+        pass
+    if (isRecCicle == True and isCircle == True):
+        moveCircle()
+
+    delay(0.001)
 
 close_canvas()
+
+
+# def drawRec():
+#     global x
+#     global y
+    
+#     if x >= 790 and y < 550:
+#         y = y + 2
+#     elif x < 790:
+#         x = x + 2
+
+#     if y >= 550:
+#         x = x - 4
+#         if x < 80:
+#             y = y-4
+#     elif y > 90 and x < 80:
+#         y = y -4
